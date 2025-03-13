f80556f67a73a59424bcb69c23143911b04d0754
Alberte <ally0002@stud.kea.dk>
2025-03-13 13:34:48 +0100

    fetgtg

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,3 +10,26 @@ export default defineConfig({
   },
   integrations: [alpinejs()],
 });
+
+scroll(animate(".parallaxgrafik1", { y: [400, -600] }), {
+  target: document.querySelector(".parallaxgrafik"),
+  offset: ["start end", "end start"],
+});
+scroll(animate(".parallaxgrafik2", { x: [-100, 100], y: [800, -1800] }), {
+  target: document.querySelector(".parallaxgrafik"),
+  offset: ["start end", "end start"],
+});
+scroll(animate(".parallaxgrafik3", { x: [-1500, 2000], y: [400, 0] }), {
+  target: document.querySelector(".parallaxgrafik"),
+  offset: ["start end", "end start"],
+});
+	
+
+	scroll(animate(".progress-bar", { scaleX: [0, 1] }));
+
+  scroll(
+    animate("#horisontalliste", {
+      transform: ["none", "translateX(-500vw)"],
+    }),
+    { target: document.querySelector("#horisontalscrollsection") }
+  ); 
